68a5488cf6fe458579d7741942310a873cf7eeb0
Adamp799 <adampeachey@virginmedia.com>
2025-12-02 11:40:43 +0000

    algo adam

---
+++ day2/part1/algorithm.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+DATA_PATH = Path(__file__).parent / "data1.txt"
+
+def read_input(path=DATA_PATH):
+    try:
+        with open(path, "r") as f:
+            id_ranges = [id_range.strip() for id_range in f.read().strip().split(",")]
+            return id_ranges
+    except FileNotFoundError:
+        return []
+
+def id_check(input_data=None):
+    if input_data is None: return 0
+    invalid_sum = 0
+    for id_range in input_data:
+        max_id = int(id_range.split("-")[1])
+        min_id = int(id_range.split("-")[0])
+        for id_num in range(min_id, max_id + 1):
+            s = str(id_num)
+            if bool(s) and s in (s + s)[1:-1]:
+                invalid_sum += id_num
+    return invalid_sum
+
+if __name__ == "__main__":
+    input_data = read_input()
+    print("Invalid ID sum:", id_check(input_data))
