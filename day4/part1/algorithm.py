b0f244134223bf390e7346166b318963b2cc5aa3
Adamp799 <adampeachey@virginmedia.com>
2025-12-04 13:31:33 +0000

    day 4 continued

--- day4/part1/algorithm.py
+++ day4/part1/algorithm.py
@@ -10,31 +10,32 @@ def read_input(path=DATA_PATH):
 
 def count_rolls(input_data=None):
     if input_data is None: return 0
-    height = len(input_data) 
-    width = len(input_data[0]) 
-    count_arr = [[0 * width] * height]
-    
-    curr_row = 0
-    curr_col = 0
-    unaccessible_count = 0
-    rolls_count = 0
+    height = len(input_data)
+    width = len(input_data[0])
+    count_arr = [[0] * width for _ in range(height)]
+    curr_row = 0; curr_col = 0
     for row in input_data:
         for char in row:
             if char == '@':
-                rolls_count += 1
-                count_arr[min(curr_row + 1, height)][curr_col] += 1
-                count_arr[min(curr_row + 1, height)][min(curr_col + 1, width)] += 1
-                count_arr[min(curr_row + 1, height)][max(curr_col - 1, 0)] += 1
-                count_arr[curr_row][min(curr_col + 1, width)] += 1
-                count_arr[curr_row][max(curr_col - 1, 0)] += 1
-                count_arr[max(curr_row - 1, 0)][curr_col] += 1
-                count_arr[max(curr_row - 1, 0)][min(curr_col + 1, width)] += 1
-                count_arr[max(curr_row - 1, 0)][max(curr_col - 1, 0)] += 1
+                safe_list_increment(count_arr, curr_row + 1, curr_col)
+                safe_list_increment(count_arr, curr_row + 1, curr_col + 1)
+                safe_list_increment(count_arr, curr_row + 1, curr_col - 1)
+                safe_list_increment(count_arr, curr_row, curr_col + 1)
+                safe_list_increment(count_arr, curr_row, curr_col - 1)
+                safe_list_increment(count_arr, curr_row - 1, curr_col)
+                safe_list_increment(count_arr, curr_row - 1, curr_col + 1)
+                safe_list_increment(count_arr, curr_row - 1, curr_col - 1)
             curr_col += 1
-            if count_arr[curr_row][curr_col] == 4: 
-                unaccessible_count += 1
+        curr_col = 0
         curr_row += 1
-    return rolls_count - unaccessible_count
+    print(count_arr)
+    return sum(1 for row in count_arr for count in row if count < 4)
+
+def safe_list_increment(count_arr, i1, i2):
+    if i1 is None or i2 is None:
+        return
+    if 0 <= i1 < len(count_arr) and 0 <= i2 < len(count_arr[i1]):
+        count_arr[i1][i2] += 1
 
 if __name__ == "__main__":
     input_data = read_input()
