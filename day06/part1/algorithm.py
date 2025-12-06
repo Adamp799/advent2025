a7130ca1b90217d5ee35327a019870e97394052c
Adamp799 <adampeachey@virginmedia.com>
2025-12-06 19:26:24 +0000

    day 6 progress

---
+++ day06/part1/algorithm.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+DATA_PATH = Path(__file__).parent / "example.txt"
+
+def read_input(path=DATA_PATH):
+    try:
+        with open(path, "r") as f:
+            lines = [line.strip().split() for line in f if line.strip()]
+            return lines
+    except FileNotFoundError:
+        return []
+
+def perform_calculations(input_data):
+    total = 0 
+    for i in range(len(input_data[0])): 
+        if input_data[3][i] == "+":
+            total += int(input_data[0][i]) + int(input_data[1][i]) + int(input_data[2][i])
+        else: total += int(input_data[0][i]) * int(input_data[1][i]) * int(input_data[2][1])
+    return total
+
+if __name__ == "__main__":
+    input_data = read_input()
+    print(f"Read {input_data} from data")
+    print("Grand Total:", perform_calculations(input_data))
