102b3e8e4531e95befc6b87664c3db03076b54b4
Adamp799 <adampeachey@virginmedia.com>
2025-12-07 11:59:03 +0000

    day7 part1 clean

--- day07/part1/algorithm.py
+++ day07/part1/algorithm.py
@@ -9,22 +9,18 @@ def read_input(path=DATA_PATH):
         return []
 
 def tachyon_beam(input_data):
+    total_splits = 0
     beam_locations = set()
     beam_locations.add(input_data[0].index('S'))
-    timelines_triangle = {input_data[0].index('S') : 1}
     for line in input_data[1:]:
         splitters = {i for i, char in enumerate(line) if char == '^'}
         intersections = beam_locations.intersection(splitters)
+        total_splits += len(intersections)
         beam_locations.update({y for x in intersections for y in (x - 1, x + 1) if 0 <= y <= len(line) - 1})
         beam_locations.difference_update(intersections)
-        for i in intersections: 
-            temp = timelines_triangle 
-            timelines_triangle.clear()
-            timelines_triangle[i - 1] = temp.get(i - 1, 0) + temp[i]
-            timelines_triangle[i + 1] = temp.get(i + 1, 0) + temp[i]
-    return sum(timelines_triangle.values())
+    return total_splits
 
 if __name__ == "__main__":
     input_data = read_input()
     print(f"Read {len(input_data)} lines from data")
-    print("Total Timelines:", tachyon_beam(input_data))
+    print("Total Splits:", tachyon_beam(input_data))
---
+++ day07/part2/algorithm.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+DATA_PATH = Path(__file__).parent / "example.txt"
+
+def read_input(path=DATA_PATH):
+    try:
+        with open(path, "r") as f:
+            return [line.strip() for line in f if line.strip()]
+    except FileNotFoundError:
+        return []
+
+def tachyon_beam(input_data):
+    beam_locations = set()
+    beam_locations.add(input_data[0].index('S'))
+    timelines_triangle = {input_data[0].index('S') : 1}
+    for line in input_data[1:]:
+        splitters = {i for i, char in enumerate(line) if char == '^'}
+        intersections = beam_locations.intersection(splitters)
+        beam_locations.update({y for x in intersections for y in (x - 1, x + 1) if 0 <= y <= len(line) - 1})
+        beam_locations.difference_update(intersections)
+        print(intersections)
+        for i in intersections: 
+            temp = timelines_triangle 
+            timelines_triangle.clear()
+            timelines_triangle[i - 1] = temp.get(i - 2, 0) + temp[i]
+            timelines_triangle[i + 1] = temp.get(i + 2, 0) + temp[i]
+    return sum(timelines_triangle.values())
+
+if __name__ == "__main__":
+    input_data = read_input()
+    print(f"Read {len(input_data)} lines from data")
+    print("Total Timelines:", tachyon_beam(input_data))
