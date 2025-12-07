f58295d860a1cded4ee7b35a0392bb97b4812b75
Adamp799 <adampeachey@virginmedia.com>
2025-12-07 11:15:43 +0000

    day7 part1 progress

--- day07/part1/algorithm.py
+++ day07/part1/algorithm.py
@@ -9,13 +9,17 @@ def read_input(path=DATA_PATH):
         return []
 
 def tachyon_beam(input_data):
+    total_splits = 0
     beam_locations = set()
     beam_locations.add(input_data[0].index('S'))
     for line in input_data[1:]:
         splitters = {i for i, char in enumerate(line) if char == '^'}
+        intersections = beam_locations.intersection(splitters)
+        total_splits += len(intersections)
+        beam_locations.update({y for x in intersections for y in (x - 1, x + 1) if 0 <= y <= len(line) - 1})
+    return total_splits
 
 if __name__ == "__main__":
     input_data = read_input()
     print(f"Read {len(input_data)} lines from data")
-    print(input_data)
     print("Total Splits:", tachyon_beam(input_data))
