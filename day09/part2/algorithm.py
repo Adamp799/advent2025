d38fcddc8fda4a6c8b990c80021cbaa9b8590ace
Adamp799 <adampeachey@virginmedia.com>
2025-12-09 22:17:30 +0000

    day9 part2 solved!

--- day09/part2/algorithm.py
+++ day09/part2/algorithm.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-DATA_PATH = Path(__file__).parent / "example.txt"
+DATA_PATH = Path(__file__).parent / "data1.txt"
 
 def read_input(path=DATA_PATH):
     try:
@@ -15,10 +15,15 @@ def find_largest_rectangle(coords):
         for x2, y2 in coords[i+1:]:
             new_area = (abs(int(x2) - int(x1)) + 1) * (abs(int(y2) - int(y1)) + 1)
             if new_area > max_area: 
-                valid = True
+                valid = True; ray_count = 0
                 for j, coord in enumerate(coords): 
-                    if coord in ([x1, y1], [x2, y2], [x1, y2], [x2, y1]): continue
                     x_coord = int(coord[0]); y_coord = int(coord[1])
+                    if (((y_coord <= min(int(y1), int(y2)) + 1 and int(edge_coords[j+1][1]) >= min(int(y1), int(y2)) + 1) or 
+                        (y_coord >= min(int(y1), int(y2)) + 1 and int(edge_coords[j+1][1]) <= min(int(y1), int(y2)) + 1)) and 
+                        (x_coord == int(edge_coords[j+1][0]) and x_coord > min(int(x1), int(x2)))):
+                        ray_count += 1
+
+                    if coord in ([x1, y1], [x2, y2], [x1, y2], [x2, y1]): continue
         
                     if (   (min(int(x1), int(x2)) < x_coord < max(int(x1), int(x2)) and
                             min(int(y1), int(y2)) < y_coord < max(int(y1), int(y2))) or 
@@ -32,12 +37,13 @@ def find_largest_rectangle(coords):
                             (((y_coord == min(int(y1), int(y2)) and int(edge_coords[j+1][1]) > y_coord) or
                             (y_coord == max(int(y1), int(y2)) and int(edge_coords[j+1][1]) < y_coord) or 
                             (y_coord < min(int(y1), int(y2)) and int(edge_coords[j+1][1]) > max(int(y1), int(y2))) or 
-                            (y_coord > max(int(y1), int(y2)) and int(edge_coords[j+1][0]) < min(int(y1), int(y2)))) and 
+                            (y_coord > max(int(y1), int(y2)) and int(edge_coords[j+1][1]) < min(int(y1), int(y2)))) and 
                             (x_coord > min(int(x1), int(x2)) and x_coord < max(int(x1), int(x2))))   ):
 
                         valid = False
                         break
-                if valid: max_area = new_area
+                print(ray_count)
+                if valid and ray_count % 2 == 1: max_area = new_area; print(f"New max area {max_area} with corners ({x1},{y1}) and ({x2},{y2})")
     return max_area
             
 if __name__ == "__main__":
