8298800ca3e329c17f95984be8361800dcdc2fe7
Adamp799 <adampeachey@virginmedia.com>
2025-12-09 16:53:16 +0000

    day 09 part11 example

--- day09/part1/algorithm.py
+++ day09/part1/algorithm.py
@@ -12,13 +12,11 @@ def find_largest_rectangle(coords):
     max_area = float('-inf')
     for i, [x1, y1] in enumerate(coords):
         for x2, y2 in coords[i+1:]:
-            new_area = abs((int(x2) - int(x1) + 1) * (int(y2) - int(y1) + 1))
+            new_area = (abs(int(x2) - int(x1)) + 1) * (abs(int(y2) - int(y1)) + 1)
             if new_area > max_area: max_area = new_area
     return max_area
-            
             
 if __name__ == "__main__":
     input_data = read_input()
     print(f"Read {len(input_data)} coordinates from data")
-    print(input_data)
     print("Largest Rectangle:", find_largest_rectangle(input_data))
