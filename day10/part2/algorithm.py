c6e4d07b07fd74f0c15ad62a67282ea3afe6802a
Adamp799 <adampeachey@virginmedia.com>
2025-12-10 14:04:39 +0000

    day10 part2 progress

---
+++ day10/part2/algorithm.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from scipy.optimize import linprog
+DATA_PATH = Path(__file__).parent / "example.txt"
+
+def read_input(path=DATA_PATH):
+    try:
+        with open(path, "r") as f:
+            return [line.strip().split(' ') for line in f if line.strip()]
+    except FileNotFoundError:
+        return []
+
+def find_button_combination(manual): 
+    min_presses_list = []
+    for machine in manual:
+        joltages = [int(x) for x in machine[-1].strip("{}").split(",") if x.strip()]
+        button_wirings = [[int(x) for x in s.strip("()").split(",") if x.strip()] for s in machine[1:-1]]
+        A = [[0]*len(button_wirings) for _ in range(len(joltages))]
+        for i in range(len(button_wirings)):
+            for j in range(len(joltages)):
+                A[j][i] = 1 if j in button_wirings[i] else 0
+        res = linprog([1]*len(button_wirings), A_eq=A, b_eq=joltages, method='highs')
+        print("Optimal value:", res.fun)
+        print("Variables [a,b,c,d,e,f]:", res.x)
+        min_presses_list.append(int(res.fun))
+    return sum(min_presses_list)
+            
+if __name__ == "__main__":
+    input_data = read_input()
+    print(f"Read {len(input_data)} coordinates from data")
+    print("Total Button Presses:", find_button_combination(input_data))
